a6153128a85125c32bd33b131da073ca8faf5c76
carabajal-facundo <facarabajal@hotmail.com>
2023-04-05 09:55:28 -0300

    se realizo unas correcciones

--- js/program.js
+++ js/program.js
@@ -2,37 +2,39 @@ function generarNumero(){
     numeroMagico = Math.floor(Math.random() * (11 - 1) + 1);
 }
 function generarAdivina(){
-    let ingresarNumero = document.createElement('input');
-    ingresarNumero.className = "form-control border border-secondary mt-5";
-    ingresarNumero.id = "numeroIngresado";
-    ingresarNumero.type = "number"
-    /*ingresarNumero.minLength = '1';
-    ingresarNumero.maxLength = '2';*/
-    ingresarNumero.placeholder = "ingrese un numero del 1 al 10 para adivinar";
-
-    let botonAdivinar = document.createElement("button");
-    botonAdivinar.innerHTML = "Adivinar!"
-    botonAdivinar.className = "btn mt-3 bg-white"
-    botonAdivinar.addEventListener('click', adivinar);
-
-    seccionAdivini.insertBefore(ingresarNumero, document.getElementById('adivina').firstChild);
-    seccionAdivini.insertBefore(botonAdivinar, document.getElementById('numeroIngresado').firstChild);
+    if(noJuego){
+        let ingresarNumero = document.createElement('input');
+        ingresarNumero.className = "form-control border border-secondary mt-5";
+        ingresarNumero.id = "numeroIngresado";
+        ingresarNumero.type = "number"
+        ingresarNumero.placeholder = "ingrese un numero del 1 al 10 para adivinar";
+    
+        let botonAdivinar = document.createElement("button");
+        botonAdivinar.innerHTML = "Adivinar!"
+        botonAdivinar.className = "btn mt-3 bg-white"
+        botonAdivinar.addEventListener('click', adivinar);
+    
+        seccionAdivini.insertBefore(ingresarNumero, document.getElementById('adivina').firstChild);
+        seccionAdivini.insertBefore(botonAdivinar, document.getElementById('numeroIngresado').firstChild);
+        noJuego = false;
+    }
+    else generarAdivina();
 }
 
 function adivinar(){
     let numero = document.getElementById('numeroIngresado').value;
-    if(numero==numeroMagico) alert("Felicidades ustede adivino cual es el numero magico");
-    else if(numero > numeroMagico) alert("Perdon pero usted ingreso un numero mayor que el 'Numero magico'");
-    else alert("Perdon pero usted ingreso un numero menor que el 'Numero magico'");
+    if(numero > 0 && numero < 11){
+        if(numero==numeroMagico) alert("Felicidades ustede adivino cual es el numero magico");
+        else if(numero > numeroMagico) alert("Perdon pero usted ingreso un numero mayor que el 'Numero magico'");
+        else alert("Perdon pero usted ingreso un numero menor que el 'Numero magico'");
+    }
+    else alert("Por favor ingrese un numero entre 1 y 10");
 }
 
 let numeroMagico;
 
+let noJuego = true;
+
 let boton = document.getElementById('iniciarJuego');
 
 let seccionAdivini = document.getElementById("adivina");
-
-/*
-<input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" maxlength="25">
-<button class="btn btn-outline-light" type="submit"><i class="bi bi-search"></i></button>
-*/
